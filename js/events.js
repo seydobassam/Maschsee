70c2ff1de63e069f4a7372e378f4ea4962f4c086
unknown <nicommbbs@byom.de>
2019-02-20 11:57:10 +0100

    Added Header Image to Events Website

---
+++ js/events.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    // Transition effect for navbar 
+    $(window).scroll(function() {
+      // checks if window is scrolled more than 500px, adds/removes solid class
+      if($(this).scrollTop() > 440) { 
+          $('.navbar').addClass('solid');
+          $('.navbar').removeClass('navbar-dark');
+      } else {
+          $('.navbar').removeClass('solid');
+          $('.navbar').addClass('navbar-dark');
+      }
+    });
+});

71847b2f1e909d6bf8b48da6c7d9008597b33c5b
unknown <nicommbbs@byom.de>
2019-02-21 11:36:04 +0100

    Changed Hero Image on Events.html

--- js/events.js
+++ js/events.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     // Transition effect for navbar 
     $(window).scroll(function() {
       // checks if window is scrolled more than 500px, adds/removes solid class
-      if($(this).scrollTop() > 440) { 
+      if($(this).scrollTop() > window.innerHeight - 60) { 
           $('.navbar').addClass('solid');
           $('.navbar').removeClass('navbar-dark');
       } else {

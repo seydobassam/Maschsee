4400602a3a1f1bbb15020d4612d0c3692b49b92b
NicoMMBBS <nicommbbs@byom.de>
2019-05-21 19:54:30 +0200

    Updated History Folders + removed unused Folders. Changes to Events

--- js/nav.js
+++ js/nav.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
     // Transition effect for navbar 
     $(window).scroll(function() {
       // checks if window is scrolled more than 500px, adds/removes solid class
-      if($(this).scrollTop() > window.innerHeight - 60) { 
+      if($(this).scrollTop() >= 300) { 
           $('.navbar').addClass('solid');
-          $('.navbar').removeClass('navbar-dark');
+          $('.navbar').removeClass('navbar-light');
       } else {
           $('.navbar').removeClass('solid');
-          $('.navbar').addClass('navbar-dark');
+          $('.navbar').addClass('navbar-light');
       }
     });
 });
